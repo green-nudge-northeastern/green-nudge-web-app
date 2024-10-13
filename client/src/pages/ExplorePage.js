c8f54e503f6e36a1cae80e5b8d8b0b6d7e0f896f
HattyHe <hatty.he@outlook.com>
2024-10-12 21:53:26 -0700

    add PeerCaseStudies Page

---
+++ client/src/pages/ExplorePage.js
@@ -0,0 +1,176 @@
+import React from 'react'
+import './ExplorePage.css'
+import { auth } from '../services/firebaseConfig'
+import { useAuthState } from 'react-firebase-hooks/auth'
+
+const Explore = () => {
+    const [user] = useAuthState(auth)
+    return (
+        <div className='explore-container'>
+            <h1>Explore Peer Case Studies</h1>
+            <div className='explore-main'>
+                <div className='explore-left'>
+                    <div className='explore-left-card'>
+                        <div>Design</div>
+                        <div>Innovation</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Process</div>
+                        <div>Innovation</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Material</div>
+                        <div>Innovation</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Energy</div>
+                        <div>Efficiency</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Digital</div>
+                        <div>Transformation</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Product Life Cycle</div>
+                        <div>Extension</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Renewable</div>
+                        <div>Energy</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Waste</div>
+                        <div>Recovery</div>
+                    </div>
+                    <div className='explore-left-card'>
+                        <div>Sharing</div>
+                        <div>Models</div>
+                    </div>
+                </div>
+                <div className='explore-right'>
+                    <h3>
+                        Global value chain for Textile industry has a growing
+                        trend in design innovation that has transformed the
+                        market and consumer dynamics.
+                    </h3>
+                    <h3>
+                        Over 70% companies are using circular design for their
+                        products resulting in durable and customizable options
+                    </h3>
+                    <div className='explore-title-box'>
+                        Case Studies. Peers in your market
+                    </div>
+                    <div className='explore-card-list'>
+                        <div className='explore-card'>
+                            <div className='explore-card-header'>
+                                <div className='explore-card-header-left'>
+                                    <div className='explore-card-header-circle'>
+                                        A
+                                    </div>
+                                    <div className='explore-card-header-nameBox'>
+                                        <div className='explore-card-header-name'>
+                                            Header
+                                        </div>
+                                        <div className='explore-card-header-sub'>
+                                            Subhead
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='explore-card-header-right'></div>
+                            </div>
+                            <div className='explore-card-img'></div>
+                            <div className='explore-card-title'>
+                                <div className='explore-card-title-first'>
+                                    Title
+                                </div>
+                                <div>Subtitle</div>
+                            </div>
+                            <div className='explore-card-description'>
+                                Lorem ipsum dolor sit amet,consectetur
+                                adipiscing elit,sed do eiusmod tempor
+                            </div>
+                            <div className='explore-card-btnList'>
+                                <div className='explore-card-btn cancel'>
+                                    Enabled
+                                </div>
+                                <div className='explore-card-btn'>Enabled</div>
+                            </div>
+                        </div>
+                        <div className='explore-card'>
+                            <div className='explore-card-header'>
+                                <div className='explore-card-header-left'>
+                                    <div className='explore-card-header-circle'>
+                                        A
+                                    </div>
+                                    <div className='explore-card-header-nameBox'>
+                                        <div className='explore-card-header-name'>
+                                            Header
+                                        </div>
+                                        <div className='explore-card-header-sub'>
+                                            Subhead
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='explore-card-header-right'></div>
+                            </div>
+                            <div className='explore-card-img'></div>
+                            <div className='explore-card-title'>
+                                <div className='explore-card-title-first'>
+                                    Title
+                                </div>
+                                <div>Subtitle</div>
+                            </div>
+                            <div className='explore-card-description'>
+                                Lorem ipsum dolor sit amet,consectetur
+                                adipiscing elit,sed do eiusmod tempor
+                            </div>
+                            <div className='explore-card-btnList'>
+                                <div className='explore-card-btn cancel'>
+                                    Enabled
+                                </div>
+                                <div className='explore-card-btn'>Enabled</div>
+                            </div>
+                        </div>
+                        <div className='explore-card'>
+                            <div className='explore-card-header'>
+                                <div className='explore-card-header-left'>
+                                    <div className='explore-card-header-circle'>
+                                        A
+                                    </div>
+                                    <div className='explore-card-header-nameBox'>
+                                        <div className='explore-card-header-name'>
+                                            Header
+                                        </div>
+                                        <div className='explore-card-header-sub'>
+                                            Subhead
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className='explore-card-header-right'></div>
+                            </div>
+                            <div className='explore-card-img'></div>
+                            <div className='explore-card-title'>
+                                <div className='explore-card-title-first'>
+                                    Title
+                                </div>
+                                <div>Subtitle</div>
+                            </div>
+                            <div className='explore-card-description'>
+                                Lorem ipsum dolor sit amet,consectetur
+                                adipiscing elit,sed do eiusmod tempor
+                            </div>
+                            <div className='explore-card-btnList'>
+                                <div className='explore-card-btn cancel'>
+                                    Enabled
+                                </div>
+                                <div className='explore-card-btn'>Enabled</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Explore
--- client/src/pages/LaunchPage.js
+++ client/src/pages/LaunchPage.js
@@ -1,68 +1,85 @@
-import React, { useState } from 'react';
-import './LaunchPage.css'; // Custom styles for the page
-import { QuestionBlock, OptionsBlock } from '../components/forms/QuestionOptionsBlock'; // Import both components
-import questionsOptionsData from '../assets/data/LaunchSurvey'; 
-
+import React, { useState, useEffect } from 'react'
+import './LaunchPage.css' // Custom styles for the page
+import {
+    QuestionBlock,
+    OptionsBlock,
+} from '../components/forms/QuestionOptionsBlock' // Import both components
+import questionsOptionsData from '../assets/data/LaunchSurvey'
+import Explorepage from './ExplorePage'
 
 const LaunchPage = () => {
-  const [selectedQuestion, setSelectedQuestion] = useState(null); // State to track the selected question
-  const [selectedOptions, setSelectedOptions] = useState({}); // State to track selected options for each category
+    const [selectedQuestion, setSelectedQuestion] = useState(null) // State to track the selected question
+    const [selectedOptions, setSelectedOptions] = useState({}) // State to track selected options for each category
+    const [showcaseItems, setShowcaseItems] = useState(false)
 
-  // Handles when a question is clicked
-  const handleQuestionClick = (category) => {
-    setSelectedQuestion(category === selectedQuestion ? null : category); // Toggle the question selection
-  };
+    // Handles when a question is clicked
+    const handleQuestionClick = (category) => {
+        setSelectedQuestion(category === selectedQuestion ? null : category) // Toggle the question selection
+    }
 
-  const handleOptionClick = (category, option) => {
-    setSelectedOptions((prevSelected) => {
-      const newSelected = prevSelected[category]?.includes(option)
-        ? prevSelected[category].filter((item) => item !== option)
-        : [...(prevSelected[category] || []), option];
+    const handleOptionClick = (category, option) => {
+        setSelectedOptions((prevSelected) => {
+            const newSelected = prevSelected[category]?.includes(option)
+                ? prevSelected[category].filter((item) => item !== option)
+                : [...(prevSelected[category] || []), option]
 
-      return { ...prevSelected, [category]: newSelected };
-    });
-  };
+            return { ...prevSelected, [category]: newSelected }
+        })
+    }
 
-  return (
-    <div className="launch-page">
-      <h1 className="launch-title">Launch a decarbonization program for you!</h1>
-      <div className="launch-grid">
-        {/* add a panel for switching between previous and next question */}
+    useEffect(() => {
+        if (selectedOptions.tools) {
+            setShowcaseItems(
+                selectedOptions.tools.includes('Peer Case Studies')
+            )
+        }
+    }, [selectedOptions])
 
-        {/* all questions */}
-        <div className="launch-questions">
-          {questionsOptionsData.map(({ question, category }) => (
-            <QuestionBlock
-              key={category}
-              question={question}
-              category={category}
-              selectedQuestion={selectedQuestion}
-              handleQuestionClick={handleQuestionClick}
-            />
-          ))}
+    return (
+        <div className='launch-page'>
+            <h1 className='launch-title'>
+                Launch a decarbonization program for you!
+            </h1>
+            <div className='launch-grid'>
+                {/* add a panel for switching between previous and next question */}
+
+                {/* all questions */}
+                <div className='launch-questions'>
+                    {questionsOptionsData.map(({ question, category }) => (
+                        <QuestionBlock
+                            key={category}
+                            question={question}
+                            category={category}
+                            selectedQuestion={selectedQuestion}
+                            handleQuestionClick={handleQuestionClick}
+                        />
+                    ))}
+                </div>
+
+                {/* all options for the selected question */}
+                <div className='launch-options'>
+                    <p className='launch-options-description'>
+                        Select all that apply. <br />
+                        The more we know, the better insights you'll receive.
+                    </p>
+                    {questionsOptionsData
+                        .filter(({ category }) => category === selectedQuestion)
+                        .map(({ category, options }) => (
+                            <OptionsBlock
+                                key={category}
+                                options={options}
+                                category={category}
+                                selectedOptions={
+                                    selectedOptions[category] || []
+                                }
+                                handleOptionClick={handleOptionClick}
+                            />
+                        ))}
+                </div>
+            </div>
+            {showcaseItems && <Explorepage></Explorepage>}
         </div>
+    )
+}
 
-        {/* all options for the selected question */}
-        <div className="launch-options">
-            <p className="launch-options-description">
-                Select all that apply. <br />
-                The more we know, the better insights you'll receive.
-            </p>
-          {questionsOptionsData
-            .filter(({ category }) => category === selectedQuestion)
-            .map(({ category, options }) => (
-              <OptionsBlock
-                key={category}
-                options={options}
-                category={category}
-                selectedOptions={selectedOptions[category] || []}
-                handleOptionClick={handleOptionClick}
-              />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default LaunchPage;
+export default LaunchPage
